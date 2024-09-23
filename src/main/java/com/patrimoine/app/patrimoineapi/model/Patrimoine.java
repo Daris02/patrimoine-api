41639b1eef24536c4241baf5383bb86205b23c62
Daris02 <hei.raymond.2@gmail.com>
2024-09-23 15:33:27 +0300

    fix: change type of dernierModification

--- src/main/java/com/patrimoine/app/patrimoineapi/model/Patrimoine.java
+++ src/main/java/com/patrimoine/app/patrimoineapi/model/Patrimoine.java
@@ -1,23 +1,39 @@
 package com.patrimoine.app.patrimoineapi.model;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 @Setter
+@ToString
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class Patrimoine {
     private int id;
+    private static int idCounter = 0; 
     private String possesseur;
-    private LocalDateTime derniereModification;
+    private String derniereModification;
 
     public Patrimoine(String possesseur) {
+        this.id = ++idCounter; 
         this.possesseur = possesseur;
-        this.derniereModification = LocalDateTime.now();
+        this.derniereModification = formatDate(LocalDateTime.now());
+    }
+
+    private String formatDate(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd' 'HH:mm");
+        return dateTime.format(formatter);
+    }
+
+    public void setDerniereModification() {
+        this.derniereModification = formatDate(LocalDateTime.now());
     }
 }
